edfd599d567da79dbace52821c234a056aa060ef
GustavAndersson <guustav.andersson@gmail.com>
2017-01-03 13:24:49 +0100

    Get recipes
    getRecipes working

--- src/java/nu/te4/beans/RecipeBean.java
+++ src/java/nu/te4/beans/RecipeBean.java
@@ -5,10 +5,54 @@
  */
 package nu.te4.beans;
 
+import com.mysql.jdbc.Connection;
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import nu.te4.support.ConnectionFactory;
+
 /**
  *
  * @author guan97005
  */
+@Stateless
 public class RecipeBean {
+    public JsonArray getRecipes() {
+        try {
+            Connection connection = ConnectionFactory.make("127.0.0.1");
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM recept";
+            ResultSet data = stmt.executeQuery(sql);
+            //arraybuilder
+            JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
+            while (data.next()) {
+                int id = data.getInt("id");
+                String name = data.getString("name");
+                int categori = data.getInt("categori_id");
+                String desc = data.getString("description");
+                String instruction = data.getString("instruction");
+                int picture = data.getInt("picture");
+
+                jsonArrayBuilder.add(Json.createObjectBuilder()
+                        .add("id", id)
+                        .add("name", name)
+                        .add("categori_id", categori)
+                        .add("description", desc)
+                        .add("instruction", instruction)
+                        .add("picture", picture).build());
+            }
+            connection.close();
+            return jsonArrayBuilder.build();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+    
+    
     
 }
--- src/java/nu/te4/services/RecipeService.java
+++ src/java/nu/te4/services/RecipeService.java
@@ -5,10 +5,46 @@
  */
 package nu.te4.services;
 
+import javax.ejb.EJB;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import nu.te4.beans.RecipeBean;
+
+
 /**
  *
  * @author guan97005
  */
+@Path("")
 public class RecipeService {
-    
+
+    @EJB
+    RecipeBean recipeBean;
+
+    @GET
+    @Path("/recipes")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRecipes() {
+        JsonArray data = recipeBean.getRecipes();
+
+        if (data == null) {
+            return Response.serverError().build();
+        }
+
+        return Response.ok(data).build();
+
+    }
+
 }
